1ba4b275b3abde08f02a9ba00e05be2cfd2fe582
Shashank Jaiswal <sameerjaiswal395@gmail.com>
2026-09-04 14:49:31 +0530

    fix fallback for unknown routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -92,6 +97,8 @@ function App() {
             element={<EngagementAnalytics />}
           />
         </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
